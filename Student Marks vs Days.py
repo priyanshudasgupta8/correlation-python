d5afd4069583fd70137c36a8e87653bac0a0bcaa
Priyanshu Dasgupta <62264051+priyanshudasgupta8@users.noreply.github.com>
2021-01-26 17:09:49 -0800

    Update Student Marks vs Days.py

--- Student Marks vs Days.py
+++ Student Marks vs Days.py
@@ -18,7 +18,7 @@ def findCorrelation(datasource):
     print("Correlation between Marks in percentage and Days present :-  \n--->",correlation[0,1])
 
 def setup():
-    data_path  = "./data/Student Marks vs Days Present.csv"
+    data_path  = "./Student Marks vs Days Present.csv"
 
     datasource = getDataSource(data_path)
     findCorrelation(datasource)
